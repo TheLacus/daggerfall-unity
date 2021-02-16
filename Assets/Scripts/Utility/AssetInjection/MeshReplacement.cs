b2151f27f3a88e1ff49e70509df765d57984aa68
TheLacus <24359151+TheLacus@users.noreply.github.com>
2021-02-16 20:51:01 +0100

    Fix regression with climate/season model variants
    Fixes #2042

--- Assets/Scripts/Utility/AssetInjection/MeshReplacement.cs
+++ Assets/Scripts/Utility/AssetInjection/MeshReplacement.cs
@@ -37,8 +37,8 @@ namespace DaggerfallWorkshop.Utility.AssetInjection
         static Func<Color32> getTreeColorCallback = () => Color.Lerp(Color.white, Color.grey, Random.value);
         static Action<Terrain> setTreesSettingsCallback = SetTreesSettings;
 
-        static HashSet<Vector2Int> triedBillboards = new HashSet<Vector2Int>();
-        static HashSet<uint> triedModels = new HashSet<uint>();
+        static readonly HashSet<Vector2Int> triedBillboards = new HashSet<Vector2Int>();
+        static readonly HashSet<(uint, ClimateBases, DaggerfallDateTime.Seasons)> triedModels = new HashSet<(uint, ClimateBases, DaggerfallDateTime.Seasons)>();
 
         #endregion
 
@@ -266,12 +266,13 @@ namespace DaggerfallWorkshop.Utility.AssetInjection
             {
                 if (ModManager.Instance != null)
                 {
-                    if (!triedModels.Contains(modelID))
+                    var key = MakeModelKey(modelID);
+                    if (!triedModels.Contains(key))
                     {
-                        if (ModManager.Instance.TryGetAsset(GetName(modelID), clone, out go))
+                        if (ModManager.Instance.TryGetAsset(GetName(key), clone, out go))
                             return true;
                         else
-                            triedModels.Add(modelID);
+                            triedModels.Add(key);
                     }
                 }
             }
@@ -304,22 +305,32 @@ namespace DaggerfallWorkshop.Utility.AssetInjection
             return false;
         }
 
+        /// <summary>
+        /// Makes a key for climate/season variant of given model id.
+        /// </summary>
+        /// <param name="modelID">Id of target model.</param>
+        /// <returns>Unique key for model variant.</returns>
+        private static (uint modelID, ClimateBases climateBase, DaggerfallDateTime.Seasons season) MakeModelKey(uint modelID)
+        {
+            if (!GameManager.HasInstance)
+                return (modelID, (ClimateBases)(-1), (DaggerfallDateTime.Seasons)(-1));
+
+            return (modelID, ClimateSwaps.FromAPIClimateBase(GameManager.Instance.PlayerGPS.ClimateSettings.ClimateType), DaggerfallUnity.Instance.WorldTime.Now.SeasonValue);
+        }
+
         ///<summary>
         /// Get all accepted names for a model ordered by priority.
         ///</summary>
-        private static string[] GetName(uint modelID)
+        private static string[] GetName((uint modelID, ClimateBases climateBase, DaggerfallDateTime.Seasons season) key)
         {
-            if (!GameManager.HasInstance)
-                return new string[] {modelID.ToString()};
+            (uint modelID, ClimateBases climateBase, DaggerfallDateTime.Seasons season) = key;
 
-            ClimateBases climateBase = ClimateSwaps.FromAPIClimateBase(GameManager.Instance.PlayerGPS.ClimateSettings.ClimateType);
-            string climateName = climateBase == ClimateBases.Desert ?
-                string.Format("{0}_{1}", modelID, climateBase):
-                string.Format("{0}_{1}{2}", modelID, climateBase, DaggerfallUnity.Instance.WorldTime.Now.SeasonValue);
+            if ((int)climateBase == -1)
+                return new string[] { modelID.ToString() };
 
             return new string[]
             {
-                climateName,
+                climateBase == ClimateBases.Desert ? $"{modelID}_{climateBase}" : $"{modelID}_{climateBase}{season}",
                 modelID.ToString()
             };
         }
